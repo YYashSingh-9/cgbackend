ab8c748a92e78600f34a8bf3c4bf3b0deb9e0163
YYashSingh-9 <yyashsngh@gmail.com>
2024-01-30 02:45:33 -0800

    more

--- Utils/CatchAsync.js
+++ Utils/CatchAsync.js
@@ -1,7 +1,7 @@
 module.exports = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => {
-      console.log("💛💛", err);
+      console.log("💛", err);
 
       return next(err);
     }); // this catch is callback runs when async task is rejected
